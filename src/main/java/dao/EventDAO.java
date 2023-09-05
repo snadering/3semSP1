7e66e907c61d499c7adce55b8570a3247dfd9a64
ChristianStennicke <113089191+ChristianStennicke@users.noreply.github.com>
2023-09-05 13:21:35 +0200

    CRUD operation til EventDAO

--- src/main/java/dao/EventDAO.java
+++ src/main/java/dao/EventDAO.java
@@ -1,8 +1,57 @@
 package dao;
 
 import config.HibernateConfig;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import model.Event;
 
 public class EventDAO {
     private EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryConfig();
+
+    public void createEvent(Event event) {
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(event);
+            em.getTransaction().commit();
+        }
+    }
+
+    public Event readEvent(int id) {
+
+        Event event;
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            event = em.find(Event.class, id);
+            em.getTransaction().commit();
+
+        }
+        return event;
+    }
+
+    public Event updateEvent(Event event) {
+
+
+        try (EntityManager em = emf.createEntityManager()) {
+            if(readEvent(event.getId())!= null) {
+                em.getTransaction().begin();
+                em.merge(event);
+                em.getTransaction().commit();
+            }
+        }
+        return event;
+    }
+
+    public void deleteEvent(int id) {
+
+        Event event;
+        try (EntityManager em = emf.createEntityManager()) {
+
+            em.getTransaction().begin();
+            event = em.find(Event.class, id);
+            if (event != null) {
+                em.remove(event);
+            }
+        }
+    }
 }
+
